e79077696472fc4721b8a65217b9833c6f5e2941
Philipp Strube <pst@kubestack.com>
2020-09-03 18:42:22 +0200

    Initialize the Git repo after extracting the starter

--- cli/repository.go
+++ cli/repository.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"path/filepath"
+	"strings"
 
+	"github.com/go-git/go-git/v5"
 	"github.com/kbst/kbst/util"
 )
 
@@ -14,6 +17,7 @@ func RepoInit(starter string, release string, path string) (err error) {
 		return err
 	}
 
+	// determine version
 	initVersion := framework.Versions[0]
 	if release != "latest" {
 		for i := range framework.Versions {
@@ -34,12 +38,14 @@ func RepoInit(starter string, release string, path string) (err error) {
 		return fmt.Errorf("'%s' is not a valid starter", starter)
 	}
 
+	// download archive
 	resp, err := util.CachedDownload(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
+	// extract archive
 	archive, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
@@ -52,6 +58,36 @@ func RepoInit(starter string, release string, path string) (err error) {
 
 	for _, name := range filenames {
 		log.Println(name)
+	}
+
+	// initialize git repository
+	repoPath, err := filepath.Abs(filenames[0])
+	if err != nil {
+		return err
+	}
+
+	repo, err := git.PlainInit(repoPath, false)
+	if err != nil {
+		return err
+	}
+
+	// make initial commit
+	wt, err := repo.Worktree()
+	if err != nil {
+		return err
+	}
+
+	copts := &git.CommitOptions{All: true}
+	msg := fmt.Sprintf("Initialized from %s starter", strings.ToUpper(starter))
+
+	_, err = wt.Add(".")
+	if err != nil {
+		return err
+	}
+
+	_, err = wt.Commit(msg, copts)
+	if err != nil {
+		return err
 	}
 
 	return
--- go.mod
+++ go.mod
@@ -10,12 +10,11 @@ require (
 	github.com/adrg/xdg v0.2.1
 	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-git/go-git/v5 v5.1.0
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
 	github.com/hashicorp/terraform-config-inspect v0.0.0-20200806211835-c481b8bfa41e
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/spf13/cobra v1.0.0
-	github.com/spf13/viper v1.7.1
 	golang.org/x/sys v0.0.0-20200826173525-f9321e4c35a6 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7
 )
