3308865bc5d105061fd472eea4ac486a4b990da8
Philipp Strube <pst@kubestack.com>
2020-09-30 11:56:10 +0200

    Pass $HOME to docker build fix #7

--- util/docker.go
+++ util/docker.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 func PathHash(path string) string {
@@ -31,6 +32,14 @@ func DockerBuild(path string, args []string) (err error) {
 	buildArgs := append([]string{"build"}, args...)
 	cmd := exec.Command("docker", buildArgs...)
 	cmd.Env = []string{"DOCKER_BUILDKIT=1"}
+	if runtime.GOOS == "darwin" {
+		// temp workaround can be removed once upstream fix is released
+		// https://github.com/kbst/kbst/issues/7
+		envHome, exists := os.LookupEnv("HOME")
+		if exists {
+			cmd.Env = append(cmd.Env, fmt.Sprintf("HOME=%s", envHome))
+		}
+	}
 	cmd.Dir = path
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
