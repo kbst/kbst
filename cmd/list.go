051c8778520144878d6eebe6b37ba0f1558984d9
Philipp Strube <pst@kubestack.com>
2023-03-29 01:23:12 +0200

    Add flag to list all modules

--- cmd/list.go
+++ cmd/list.go
@@ -18,12 +18,18 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"os"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/kbst/kbst/pkg/stack"
 	"github.com/kbst/kbst/pkg/tfhcl"
 	"github.com/kbst/kbst/pkg/util"
 	"github.com/spf13/cobra"
+	"golang.org/x/exp/slices"
 )
+
+var showAll bool
 
 var listCmd = &cobra.Command{
 	Use:   "list",
@@ -42,24 +48,57 @@ var listCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 
+		w := tabwriter.NewWriter(os.Stdout, 4, 8, 2, '\t', 0)
+		line := "%s\t%s\t%s\n"
+
+		filterCustomModules := []string{}
 		for _, c := range s.Clusters {
-			fmt.Println(c.Name())
+			fmt.Fprintf(w, line, "TYPE", "NAME", "VERSION")
+			fmt.Fprintf(w, line, "cluster", c.Name(), c.Version)
 
 			for _, np := range s.NodePools {
 				if np.ClusterName == c.Name() {
-					fmt.Println(np.Name())
+					fmt.Fprintf(w, line, "node-pool", np.Name(), np.Version)
 				}
 			}
 
 			for _, svc := range s.Services {
 				if svc.ClusterName == c.Name() {
-					fmt.Println(svc.Name())
+					fmt.Fprintf(w, line, "service", svc.Name(), svc.Version)
+				}
+			}
+
+			if showAll {
+				for _, m := range s.Modules {
+					if strings.HasPrefix(m.Name(), fmt.Sprintf("%s_", c.Name())) {
+						// if custom modules are prefixed
+						// with the name of a cluster show them here
+						filterCustomModules = append(filterCustomModules, m.Name())
+						fmt.Fprintf(w, line, "custom", m.Name(), "")
+					}
+				}
+			}
+
+			fmt.Fprintf(w, line, "", "", "")
+		}
+
+		// show custom modules that were not
+		// prefixed with a cluster name
+		if showAll && len(s.Modules) > 0 {
+			fmt.Fprintf(w, line, "TYPE", "NAME", "VERSION")
+			for _, m := range s.Modules {
+				if !slices.Contains(filterCustomModules, m.Name()) {
+					fmt.Fprintf(w, line, "custom", m.Name(), "")
 				}
 			}
 		}
+
+		w.Flush()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+
+	listCmd.Flags().BoolVarP(&showAll, "all", "a", false, "List all modules")
 }
